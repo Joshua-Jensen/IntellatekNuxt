025c1c833a3ca752d76f519e653cf9657a5c6513
josh <joshjj3@gmail.com>
2024-03-05 15:14:31 -0700

    forms are fun

--- pages/Contact.vue
+++ pages/Contact.vue
@@ -1,27 +1,31 @@
 <template>
   <div class="container-fluid position-relative p-0 text-color">
-    <div class="row">
-      <div class="col-12 bg-img mb-5">
-      </div>
-      <div class="text-box text-center">
-        <h2>Contact Us</h2>
-        <div class="">
-          <h6 class=""><router-link to="/" class="no-hyperlink">Home</router-link> /services</h6>
+    <div class="row justify-content-center">
+      <div class="col-8">
+        <div class="row">
+          <div class="col-12 bg-img mb-5">
+          </div>
+          <div class="text-box text-center">
+            <h2>Contact Us</h2>
+            <div class="">
+              <h6 class=""><router-link to="/" class="no-hyperlink">Home</router-link> /services</h6>
+            </div>
+          </div>
+          <div class="col-md-4 align-items-center row padding">
+            <h6 class="">Phone</h6>
+            <a class="mb-2 no-hyperlink" href="tel:+1702558-8324">(702) 558-8324</a>
+            <h6>Email</h6>
+            <a class="mb-2 no-hyperlink" href="mailto:support@intellatek.net">support@intellatek.net</a>
+            <h6 class="">Address</h6>
+            <p>1378 Paseo Verde Pkwy #200, Henderson, NV 89012</p>
+          </div>
+          <div class="col-md-8">
+            <img class="placeholder" src="/intellatekMaps.png" alt="">
+          </div>
         </div>
-      </div>
-      <div class="col-md-4 align-items-center row padding">
-        <h6 class="">Phone</h6>
-        <a class="mb-2 no-hyperlink" href="tel:+1702558-8324">(702) 558-8324</a>
-        <h6>Email</h6>
-        <a class="mb-2 no-hyperlink" href="mailto:support@intellatek.net">support@intellatek.net</a>
-        <h6 class="">Address</h6>
-        <p>1378 Paseo Verde Pkwy #200, Henderson, NV 89012</p>
-      </div>
-      <div class="col-md-8">
-        <img class="placeholder" src="/intellatekMaps.png" alt="">
+        <Contact-Email />
       </div>
     </div>
-    <ContactEmail />
   </div>
 </template>
 
