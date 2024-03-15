9a6a4ef29ffa63a161e241f1c6a23c26be3b8a3a
josh jensen <joshjj3@gmail.com>
2024-03-15 11:38:39 -0600

    make sure it is all setup

---
+++ models/SchoolsModel.js
@@ -0,0 +1,15 @@
+export class Schools{
+  constructor(data){
+    this.name = data.name,
+    this.location = data.location,
+    this.photo = data.photo,
+    this.about = data.about,
+    this.link = data.link,
+    this.organization = data.organization
+  }
+
+  
+}
+
+
+
